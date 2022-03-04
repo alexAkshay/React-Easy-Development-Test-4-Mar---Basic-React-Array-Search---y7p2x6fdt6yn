c15eb0f946983d06505fc53acb50f5988bde0d5e
alexAkshay <93480740+alexAkshay@users.noreply.github.com>
2022-03-04 18:53:15 +0530

    Update one

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react/cjs/react.production.min";
 
-import '../styles/App.css';
+import "../styles/App.css";
 
-
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
+const searchArray = [
+  "Newton",
+  "School",
+  "Newton School",
+  "React",
+  "Preact",
+  "Node",
+  "Mongoose",
+  "Angular",
+  "Vue",
+  "Apple",
+  "Microsoft",
+  "Netflix",
+  "Meta",
+];
 const App = () => {
-
-  
+  const [list, setList] = useState("");
+  const handleChange = (event) => {
+    setList(event.target.value);
+  };
   return (
     <div id="main">
-      
+      <input id="search-input" type="text" onChange={handleChange} />
+      <ul>
+        {list.map((data) => {
+          return <li>{data}</li>;
+        })}
+      </ul>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
